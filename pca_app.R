152a9866f9da0f2d6c31e912b832418c4ad81e0a
agustin-alesso <calesso@fca.unl.edu.ar>
2020-09-14 16:21:07 -0500

    last changes (feb)

--- pca_app.R
+++ pca_app.R
@@ -19,7 +19,8 @@ library(Matrix)
 ui <- fluidPage(
     
     # Application title
-    titlePanel("Playing with Principal Components"),
+    titlePanel("Principal Components Simulator | CPSC 543", 
+               windowTitle = "PCA simulator | CPSC543"),
     
     # Instructions
     h3("Instructions:"),
@@ -54,7 +55,7 @@ ui <- fluidPage(
                 column(2, numericInput(inputId = "sigma3", label = NULL, value = 1, min = 0.1, step = 0.05)),
                 column(2, numericInput(inputId = "sigma4", label = NULL, value = 1, min = 0.1, step = 0.05))
             ),
-            p("3. Define the correlation matrix matrix"),
+            p("3. Define the population correlation matrix (upper triangle only)"),
             fluidRow(
                 column(3, offset = 1, "Y2"),
                 column(3, "Y3"),
@@ -62,30 +63,39 @@ ui <- fluidPage(
             ),
             fluidRow(
                 column(1, "Y1"),
-                column(3, numericInput(inputId = "r12", label = NULL, value = 1, step = 0.05, min = -1, max = 1)),
-                column(3, numericInput(inputId = "r13", label = NULL, value = 1, step = 0.05, min = -1, max = 1)),
-                column(3, numericInput(inputId = "r14", label = NULL, value = 1, step = 0.05, min = -1, max = 1))
+                column(3, numericInput(inputId = "r12", label = NULL, value = 0, step = 0.05, min = -1, max = 1)),
+                column(3, numericInput(inputId = "r13", label = NULL, value = 0, step = 0.05, min = -1, max = 1)),
+                column(3, numericInput(inputId = "r14", label = NULL, value = 0, step = 0.05, min = -1, max = 1))
             ),
             fluidRow(
                 column(1, "Y2"),
-                column(3, offset = 3, numericInput(inputId = "r23", label = NULL, value = 1, step = 0.05, min = -1, max = 1)),
-                column(3, numericInput(inputId = "r24", label = NULL, value = 1, step = 0.05, min = -1, max = 1))
+                column(3, offset = 3, numericInput(inputId = "r23", label = NULL, value = 0, step = 0.05, min = -1, max = 1)),
+                column(3, numericInput(inputId = "r24", label = NULL, value = 0, step = 0.05, min = -1, max = 1))
             ),
             fluidRow(
                 column(1, "Y3"),
-                column(3, offset = 6, numericInput(inputId = "r34", label = NULL, value = 1, step = 0.05, min = -1, max = 1))
+                column(3, offset = 6, numericInput(inputId = "r34", label = NULL, value = 0, step = 0.05, min = -1, max = 1))
             ),
-            numericInput(inputId = "n",  label = "sample size", value = 30),
-            checkboxInput(inputId = "cormat", label = "Use correlation matrix", value = T),
+            p("4. Choose the sample size to be drawn from the multivariate normal distribution"),
+            numericInput(inputId = "n",  label = NULL, value = 30),
             
-            #h5("Population variance-covariance matrix"),
-            #verbatimTextOutput('pop_vcov'),
-            #h5("Population Correlation matrix"),
-            #verbatimTextOutput('pop_r')
+            h5("More options"),
+            checkboxInput(inputId = "cormat", label = "Use correlation matrix for PCA", value = T),
+            checkboxInput(inputId = "popmats", label = "Show population matrices", value = F),
+            checkboxInput(inputId = "fixed_axis", label = "Use fixed limits in biplots (-0.5, 0.5)", value = T)
+            #numericInput(inputId = "scale",  label = "Scale biplot", value = 1, min = 0, max = 1, step = 0.1)
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
+            
+            conditionalPanel(
+                condition = "input.popmats == true",
+                h5("Population variance-covariance matrix"),
+                verbatimTextOutput('pop_vcov'),
+                h5("Population Correlation matrix"),
+                verbatimTextOutput('pop_r')
+            ),
             h5("Sample variance-covariance matrix"),
             verbatimTextOutput('sample_vcov'),
             h5("Sample correlation matrix"),
@@ -109,37 +119,46 @@ server <- function(input, output) {
     # Get pop mean vector
     mu <- reactive(c(input$mu1, input$mu2, input$mu3, input$mu4))
     
-    Sigma <- reactive({
-        
-        # Get pop sd vector
-        D <- diag(c(input$sigma1, input$sigma2, input$sigma3, input$sigma4))
-        
+    R <- reactive({
         # Create positive definite R matrix
         rij <- c(input$r12, input$r13, input$r23, input$r14, input$r24, input$r34)
         # rij <- jitter(rij, factor = 0.0001)
         R <- matrix(1, ncol = 4, nrow = 4)
         R[upper.tri(R)] <- rij
         R <- t(R)
-        R[lower.tri(R)] <- rij
-        
+        R[upper.tri(R)] <- rij
+    
         if (det(R) > 0) {
             showNotification("Positive definite matrix", duration = 15)
         } else {    
             showNotification("Non-positive definite matrix, using the nearest PD matrix", duration = 15, type = "warning")
-            R <- nearPD(R, corr = T)$mat
+            R <- as.matrix(nearPD(R, corr = T)$mat)
         }
         
+        colnames(R) <- rownames(R) <- paste0("Y", 1:4)
+        R
+        
+    })
+    
+    
+    Sigma <- reactive({
+        
+        # Get pop sd vector
+        D <- diag(c(input$sigma1, input$sigma2, input$sigma3, input$sigma4))
+        
         # Get sigma matrix
-        Sigma <- D %*% R %*% D
+        Sigma <- D %*% R() %*% D
+        Sigma <- as.matrix(Sigma)
+        colnames(Sigma) <- rownames(Sigma) <- paste0("Y", 1:4)
         Sigma
     })
     
-    #output$pop_r <- as.matrix(R)
-    #output$pop_vcov <- Sigma
+    output$pop_r <- renderPrint(R())
+    output$pop_vcov <- renderPrint(Sigma())
     
     # Simulate data
     data <- reactive({
-        Y <- mvrnorm(n = input$n, mu = mu(), Sigma = Sigma())
+        Y <- mvrnorm(n = input$n, mu = mu(), Sigma = Sigma(), empirical = F)
         Y <- data.frame(Y)
         colnames(Y) <- paste0("Y", 1:4)
         Y
@@ -163,8 +182,20 @@ server <- function(input, output) {
     # Create biplot
     output$biplot <- renderPlot({
         
-        # Extract PCs
-        biplot(pca())
+        pts_labs <- rep(".", input$n)
+        lims <- c(-0.5, 0.5)
+        
+        # Plot PCs
+        par(mfcol = c(1,2))
+        if (input$fixed_axis) {
+            biplot(pca(), choices = c(1, 2), xlabs = pts_labs, cex = c(1.5, 1),
+                   xlim = lims, ylim = lims)
+            biplot(pca(), choices = c(1, 3), xlabs = pts_labs, cex = c(1.5, 1),
+                   xlim = lims, ylim = lims)
+        } else {
+        biplot(pca(), choices = c(1, 2), xlabs = pts_labs, cex = c(1.5, 1))
+        biplot(pca(), choices = c(1, 3), xlabs = pts_labs, cex = c(1.5, 1))
+        }
         
     }) 
     
